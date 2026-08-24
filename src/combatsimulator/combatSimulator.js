8b0a9208e43594deff26d129db97a174b4901d09
Seal773 <wanglizhoujerry@gmail.com>
2026-08-23 21:30:18 -0400

    fix: timestamp enrage buffs

--- src/combatsimulator/combatSimulator.js
+++ src/combatsimulator/combatSimulator.js
@@ -1127,7 +1127,7 @@ class CombatSimulator extends EventTarget {
                     "startTime": "0001-01-01T00:00:00Z",
                     "duration": ENRAGE_TICK_INTERVAL
             };
-            enemy.addBuffs([enrageDamageBuff, enrageAccuracyBuff]);
+            enemy.addBuffs([enrageDamageBuff, enrageAccuracyBuff], this.simulationTime);
 
             this.simResult.maxEnrageStack = Math.max(this.simResult.maxEnrageStack, nowStack);
         }
